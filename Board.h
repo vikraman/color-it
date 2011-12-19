37ab8d52cb5b173910114325d95ed242ae7a1b84
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2011-12-19 22:57:50 +0530

    initial commit

---
+++ Board.h
@@ -0,0 +1,37 @@
+#ifndef _BOARD_H
+#define _BOARD_H
+
+#include <vector>
+#include <utility>
+
+#include "Exceptions.h" 
+
+class Board
+{
+public:
+    Board(int rows, int cols)
+        {
+            if (rows >= 2 && cols >= 2) {
+                cells.resize(rows);
+                for (int i = 0; i < rows; i++)
+                    cells[i].resize(cols);
+            }
+            else
+                throw InitException();
+        }
+    char getColorOfCell(int row, int col)
+        {
+            if (row >= 0 && row < cells.size() && col >= 0 && col < cells[0].size())
+                return cells[row][col];
+            else
+                throw InternalException();
+        }
+    std::pair<size_t,size_t> size() const
+        {
+            return std::make_pair(cells.size(), cells[0].size());
+        }
+private:
+    std::vector<std::vector<char> > cells;
+};
+
+#endif
---
+++ Exceptions.h
@@ -0,0 +1,8 @@
+#ifndef _EXCEPTIONS_H
+#define _EXCEPTIONS_H
+
+class InitException {};
+
+class InternalException {};
+
+#endif
